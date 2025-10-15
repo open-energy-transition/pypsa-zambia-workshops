0a524ca4f7ef3ceee65e23afe953ff98fc7adf6d
Daniel Rüdt <117752024+daniel-rdt@users.noreply.github.com>
2025-10-15 15:23:37 +0200

    chore: improve pre-commit and environment (#15)
    * ci: add pre-commit ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * ci: use environment.yaml file
    
    * chore: update env
    
    * ci: add preview of page
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * chore: update environment.yaml
    
    * ci: fix actions version and default run
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * ci: add cache
    
    * ci: fix permissions
    
    * fix: update link for new pypsa docs in notebook 1
    
    * ci: increase timeout
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Thomas Gilon <thomas.gilon@openenergytransition.org>

--- open-tyndp-workshops/scripts/build_data.py
+++ open-tyndp-workshops/scripts/build_data.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 
 if __name__ == "__main__":
-    
+
     # import input data
     df = pd.read_csv(snakemake.input[0])
-    
+
     # filter for import corridors for 2030 with capacity larger than 0
     df = df.query("YEAR == 2030 and `MAX CAPACITY [MW]` > 0")
 
